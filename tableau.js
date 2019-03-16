0c7ac72a50f86d45ee6125fee4e4661f2b44d35f
vishu94 <gargvishu47@gmail.com>
2019-03-16 18:52:20 +0530

    removed unnessary files

--- tableau.js
+++
@@ -1,6 +0,0 @@
-function initViz() {
-    var containerDiv = document.getElementById("tableauViz"),
-    url = "https://public.tableau.com/views/obesitydashboard_4/Obesity?:embed=y&:display_count=yes";
-
-    var viz = new tableau.Viz(containerDiv, url);
-}
